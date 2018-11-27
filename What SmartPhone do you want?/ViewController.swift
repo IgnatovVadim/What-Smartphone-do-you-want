b464eec0ac56d0694ee05d5652b01b9df492e3e1
Vadim Ignatov <appdevvadimig@gmail.com>
2018-11-27 12:07:29 +0300

    Initial Commit

---
+++ What SmartPhone do you want?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What SmartPhone do you want?
+//
+//  Created by Вадим Игнатов on 11/27/18.
+//  Copyright © 2018 Вадим Игнатов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
